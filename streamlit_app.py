cc2630066c2d59ab5386b291531b94d623cbc882
jimin2521 <20242521@sdhs.gwe.hs.kr>
2024-11-20 14:40:57 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,15 +5,11 @@ if st.button("메뉴 열기"):
     st.markdown(
         """
         <script type="text/javascript">
-            window.open("data:text/html,<html><head><title>메뉴</title></head>
-            <body>
-                 if menu == "메뉴 1":
-        st.write("메뉴 1이 선택되었습니다.")
-    elif menu == "메뉴 2":
-        st.write("메뉴 2가 선택되었습니다.")
-    elif menu == "메뉴 3":
-        st.write("메뉴 3이 선택되었습니다.")
-    elif menu == "메뉴 4":
-        st.write("메뉴 4가 선택되었습니다.")
-else:
-    st.write("왼쪽 상단의 버튼을 클릭하여 메뉴를 열 수 있습니다.")
+            window.open("data:text/html,<html><head><title>하얀 화면</title></head>
+            <body style='background-color: white;'>
+                <h2>하얀 화면입니다!</h2>
+            </body></html>", "_blank");
+        </script>
+        """, unsafe_allow_html=True
+    )
+
