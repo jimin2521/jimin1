f2a19aa14e3e80ad5dfebc550148685a2e483b85
jimin2521 <20242521@sdhs.gwe.hs.kr>
2024-11-20 14:30:03 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,14 +7,13 @@ if st.button("메뉴 열기"):
         <script type="text/javascript">
             window.open("data:text/html,<html><head><title>메뉴</title></head>
             <body>
-                <h2>메뉴 선택</h2>
-                <ul>
-                    <li><a href="#">메뉴 1</a></li>
-                    <li><a href="#">메뉴 2</a></li>
-                    <li><a href="#">메뉴 3</a></li>
-                    <li><a href="#">메뉴 4</a></li>
-                </ul>
-            </body></html>", "_blank");
-        </script>
-        """, unsafe_allow_html=True
-    )
+                 if menu == "메뉴 1":
+        st.write("메뉴 1이 선택되었습니다.")
+    elif menu == "메뉴 2":
+        st.write("메뉴 2가 선택되었습니다.")
+    elif menu == "메뉴 3":
+        st.write("메뉴 3이 선택되었습니다.")
+    elif menu == "메뉴 4":
+        st.write("메뉴 4가 선택되었습니다.")
+else:
+    st.write("왼쪽 상단의 버튼을 클릭하여 메뉴를 열 수 있습니다.")
